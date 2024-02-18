85f8733d84a346b6b370cf5504f79c21fe5bf79d
sohagmrida15 <sohagmrida15@gmail.com>
2024-02-19 05:20:59 +0600

    seat number increase and decrease function added

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -12,13 +12,15 @@ function keyBoardEvent(id) {
     const seatNumber = document.getElementById(id);
     seatNumber.addEventListener('click', function () {
         if (!selectedSeats.includes(id)) {
-            
-            // push the selected seats to array
+            if(selectedSeats.length < 4) {
+               // push the selected seats to array
             selectedSeats.push(id);
+
             // remove and add bg color to selected seat
             const clickedSeat = document.getElementById(id);
             clickedSeat.classList.remove('bg-[#bebebe]');
             clickedSeat.classList.add('bg-[#1DD100]');
+
             // decrease from total seat number
             let remainingSeat = getNumberbyId('total-seat');
             let presentRemainingSeat = remainingSeat - 1;
@@ -29,8 +31,21 @@ function keyBoardEvent(id) {
             let selectedSeatNumber = getNumberbyId('selected-seat');
             let presentSeat = selectedSeatNumber + 1;
             let curentSeatBooked = document.getElementById('selected-seat');
-            curentSeatBooked.innerText = presentSeat;
+            curentSeatBooked.innerText = presentSeat; 
+
+            // display sit name and price
+            const seat1 = document.getElementById('seat-1');
+            seat1.classList.remove('hidden');
+            const append =
+
+
+
+            
+            }
+            else{
+                alert("You Cannot buy more than Four ticket at a time");
+            }
         }
-        console.log(selectedSeats);
+        
     });
 }
