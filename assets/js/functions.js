b1ccf4e3018eb2da66a6b12f91cc0cfe58297970
sohagmrida15 <sohagmrida15@gmail.com>
2024-02-19 04:42:16 +0600

    all keyboard functions added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,9 +6,6 @@ buyTicketBtn.addEventListener('click', function(){
 });
 
 
-
-
-
 keyBoardEvent('a1');
 keyBoardEvent('a2');
 keyBoardEvent('a3');
@@ -17,5 +14,37 @@ keyBoardEvent('b1');
 keyBoardEvent('b2');
 keyBoardEvent('b3');
 keyBoardEvent('b4');
+keyBoardEvent('c1');
+keyBoardEvent('c2');
+keyBoardEvent('c3');
+keyBoardEvent('c4');
+keyBoardEvent('d1');
+keyBoardEvent('d2');
+keyBoardEvent('d3');
+keyBoardEvent('d4');
+keyBoardEvent('e1');
+keyBoardEvent('e2');
+keyBoardEvent('e3');
+keyBoardEvent('e4');
+keyBoardEvent('f1');
+keyBoardEvent('f2');
+keyBoardEvent('f3');
+keyBoardEvent('f4');
+keyBoardEvent('g1');
+keyBoardEvent('g2');
+keyBoardEvent('g3');
+keyBoardEvent('g4');
+keyBoardEvent('h1');
+keyBoardEvent('h2');
+keyBoardEvent('h3');
+keyBoardEvent('h4');
+keyBoardEvent('i1');
+keyBoardEvent('i2');
+keyBoardEvent('i3');
+keyBoardEvent('i4');
+keyBoardEvent('j1');
+keyBoardEvent('j2');
+keyBoardEvent('j3');
+keyBoardEvent('j4');
 
 
--- assets/js/utility.js
+++ assets/js/utility.js
@@ -11,22 +11,26 @@ var selectedSeats = [];
 function keyBoardEvent(id) {
     const seatNumber = document.getElementById(id);
     seatNumber.addEventListener('click', function () {
-        // push the selected seats to array
-            if (!selectedSeats.includes(id)) {
-                selectedSeats.push(id);
-            }
+        if (!selectedSeats.includes(id)) {
+            
+            // push the selected seats to array
+            selectedSeats.push(id);
+            // remove and add bg color to selected seat
+            const clickedSeat = document.getElementById(id);
+            clickedSeat.classList.remove('bg-[#bebebe]');
+            clickedSeat.classList.add('bg-[#1DD100]');
+            // decrease from total seat number
+            let remainingSeat = getNumberbyId('total-seat');
+            let presentRemainingSeat = remainingSeat - 1;
+            let curentRemainingSeat = document.getElementById('total-seat');
+            curentRemainingSeat.innerText = presentRemainingSeat;
+
+            // increase selected seat number
+            let selectedSeatNumber = getNumberbyId('selected-seat');
+            let presentSeat = selectedSeatNumber + 1;
+            let curentSeatBooked = document.getElementById('selected-seat');
+            curentSeatBooked.innerText = presentSeat;
+        }
         console.log(selectedSeats);
-        // remove and add bg color to selected seat
-        const clickedSeat = document.getElementById(id);
-        clickedSeat.classList.remove('bg-[#bebebe]');
-        clickedSeat.classList.add('bg-[#1DD100]');
-        // decrease from total seat number
-        let remainingSeat = getNumberbyId('total-seat');
-        let presentRemainingSeat = remainingSeat - 1;
-        let curentRemainingSeat = document.getElementById('total-seat');
-        curentRemainingSeat.innerText = presentRemainingSeat;
-
-
-        const selectedSeatNumber = getNumberbyId('selected-seat');
     });
 }
