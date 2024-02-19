d28a10b5135ff8b7b161bcfdacc95f64ed64f6c7
Sohagmrida15 <sohagmrida15@gmail.com>
2024-02-19 19:41:56 +0600

    Grand Total function added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,46 +5,116 @@ buyTicketBtn.addEventListener('click', function(){
     target.scrollIntoView({behavior: "smooth"});
 });
 
+// Function for get and convert text to number
+function getNumberbyId(id) {
+    const text = document.getElementById(id).innerText;
+    const number = parseInt(text);
+    return number;
+}
 
-keyBoardEvent('a1');
-keyBoardEvent('a2');
-keyBoardEvent('a3');
-keyBoardEvent('a4');
-keyBoardEvent('b1');
-keyBoardEvent('b2');
-keyBoardEvent('b3');
-keyBoardEvent('b4');
-keyBoardEvent('c1');
-keyBoardEvent('c2');
-keyBoardEvent('c3');
-keyBoardEvent('c4');
-keyBoardEvent('d1');
-keyBoardEvent('d2');
-keyBoardEvent('d3');
-keyBoardEvent('d4');
-keyBoardEvent('e1');
-keyBoardEvent('e2');
-keyBoardEvent('e3');
-keyBoardEvent('e4');
-keyBoardEvent('f1');
-keyBoardEvent('f2');
-keyBoardEvent('f3');
-keyBoardEvent('f4');
-keyBoardEvent('g1');
-keyBoardEvent('g2');
-keyBoardEvent('g3');
-keyBoardEvent('g4');
-keyBoardEvent('h1');
-keyBoardEvent('h2');
-keyBoardEvent('h3');
-keyBoardEvent('h4');
-keyBoardEvent('i1');
-keyBoardEvent('i2');
-keyBoardEvent('i3');
-keyBoardEvent('i4');
-keyBoardEvent('j1');
-keyBoardEvent('j2');
-keyBoardEvent('j3');
-keyBoardEvent('j4');
+// Function for apend seat name
+function apendSeatName(hide, apendContainer, i){
+    const seat = document.getElementById(hide);
+    seat.classList.remove('hidden');
+    const apend = document.getElementById(apendContainer);
+    apend.innerText = selectedSeats[i];
+    return apend;
+}
+
+
+// Function for keyboard event
+var selectedSeats = [];
+function keyBoardEvent(id) {
+    const seatNumber = document.getElementById(id);
+    seatNumber.addEventListener('click', function () {
+        if (!selectedSeats.includes(id)) {
+            if(selectedSeats.length < 4) {
+               // push the selected seats to array
+            selectedSeats.push(id);
+
+            // remove and add bg color to selected seat
+            const clickedSeat = document.getElementById(id);
+            clickedSeat.classList.remove('bg-[#bebebe]');
+            clickedSeat.classList.add('bg-[#1DD100]');
+
+            // decrease from total seat number
+            let remainingSeat = getNumberbyId('total-seat');
+            let presentRemainingSeat = remainingSeat - 1;
+            let curentRemainingSeat = document.getElementById('total-seat');
+            curentRemainingSeat.innerText = presentRemainingSeat;
+
+            // increase selected seat number
+            let selectedSeatNumber = getNumberbyId('selected-seat');
+            let presentSeat = selectedSeatNumber + 1;
+            let curentSeatBooked = document.getElementById('selected-seat');
+            curentSeatBooked.innerText = presentSeat; 
+
+            // display seat name, update total price and grand total
+
+            // get total price value
+            let totalPrice = document.getElementById('total-price');
+            let totalPriceText = totalPrice.innerText;
+            let totalPriceNumber = parseFloat(totalPriceText);
+            
+            // Get grand total value
+            let grandTotal = document.getElementById('grand-total');
+            let grandTotalText = grandTotal.innerText;
+            
+
+            if(selectedSeats.length == 1){
+                apendSeatName('seat-1', 'seat-name-1', 0);
+                totalPriceNumber = totalPriceNumber + 550;
+                totalPrice.innerText = totalPriceNumber;
+                grandTotal.innerText = totalPriceNumber;
+            }
+            if(selectedSeats.length == 2){
+                apendSeatName('seat-2', 'seat-name-2', 1);
+                totalPriceNumber = totalPriceNumber + 550;
+                totalPrice.innerText = totalPriceNumber;
+                grandTotal.innerText = totalPriceNumber;
+            }
+            if(selectedSeats.length == 3){
+                apendSeatName('seat-3', 'seat-name-3', 2);
+                totalPriceNumber = totalPriceNumber + 550;
+                totalPrice.innerText = totalPriceNumber;
+                grandTotal.innerText = totalPriceNumber;
+            }
+            if(selectedSeats.length == 4){
+                apendSeatName('seat-4', 'seat-name-4', 3);
+                totalPriceNumber = totalPriceNumber + 550;
+                totalPrice.innerText = totalPriceNumber;
+                grandTotal.innerText = totalPriceNumber; 
+            }
+            
+            // 
+            
+
+
+            }
+            else{
+                alert("You Cannot buy more than Four ticket at a time");
+            }
+        }
+        
+    });
+}
+
+// Function for calling keyboard events
+function callKeyBoardEvents() {
+    const rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    const cols = ['1', '2', '3', '4'];
+
+    for (const row of rows) {
+        for (const col of cols) {
+            keyBoardEvent(row + col);
+        }
+    }
+}
+
+callKeyBoardEvents();
+
+
+
+
 
 
--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,51 +1,7 @@
-// Function for get and convert text to number
-function getNumberbyId(id) {
-    const text = document.getElementById(id).innerText;
-    const number = parseInt(text);
-    return number;
-}
-
-// Empty array for selected seats
-var selectedSeats = [];
-
-function keyBoardEvent(id) {
-    const seatNumber = document.getElementById(id);
-    seatNumber.addEventListener('click', function () {
-        if (!selectedSeats.includes(id)) {
-            if(selectedSeats.length < 4) {
-               // push the selected seats to array
-            selectedSeats.push(id);
-
-            // remove and add bg color to selected seat
-            const clickedSeat = document.getElementById(id);
-            clickedSeat.classList.remove('bg-[#bebebe]');
-            clickedSeat.classList.add('bg-[#1DD100]');
-
-            // decrease from total seat number
-            let remainingSeat = getNumberbyId('total-seat');
-            let presentRemainingSeat = remainingSeat - 1;
-            let curentRemainingSeat = document.getElementById('total-seat');
-            curentRemainingSeat.innerText = presentRemainingSeat;
-
-            // increase selected seat number
-            let selectedSeatNumber = getNumberbyId('selected-seat');
-            let presentSeat = selectedSeatNumber + 1;
-            let curentSeatBooked = document.getElementById('selected-seat');
-            curentSeatBooked.innerText = presentSeat; 
-
-            // display sit name and price
-            const seat1 = document.getElementById('seat-1');
-            seat1.classList.remove('hidden');
-            const append =
 
 
 
-            
-            }
-            else{
-                alert("You Cannot buy more than Four ticket at a time");
-            }
-        }
-        
-    });
-}
+
+
+
+
